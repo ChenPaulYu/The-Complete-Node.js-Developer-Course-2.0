1d0a9b83c074c2efec2dcfce6ac295809d470b06
Paul <b10207146@mail.ntust.edu.tw>
2018-01-07 23:11:50 +0800

    add GET /todos feature and test

--- Chapter7/node-todo-api/server/tests/server.test.js
+++ Chapter7/node-todo-api/server/tests/server.test.js
@@ -4,8 +4,16 @@ const request = require('supertest')
 const { app } = require('./../server.js')
 const { Todo } = require('./../models/todo')
 
+const todos = [{
+    text : 'First text todo'
+},{
+    text : 'Second text todo'
+}]
+
 beforeEach( (done) => {
-    Todo.remove({}).then(() => done())
+    Todo.remove({}).then(() => {    
+        return Todo.insertMany(todos)
+    }).then(() => done())
 })
 
 
@@ -24,7 +32,7 @@ describe('POST /todos', () => {
                     return done(err)
                 }
 
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1)
                     expect(todos[0].text).toBe(text)
                     done()
@@ -44,7 +52,7 @@ describe('POST /todos', () => {
                     return done(err)
                 }
             Todo.find().then((todos) => {
-                expect(todos.length).toBe(0)
+                expect(todos.length).toBe(2)
                 done()
             }).catch((e) => {
                 done(e)
@@ -52,4 +60,18 @@ describe('POST /todos', () => {
                 
             })
     })
+})
+
+describe('GET /todos', () => {
+
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2)
+            })
+            .end(done)
+    })
+
 })
